a9b0685bc5cc5a9882d02d83a34a4a1c574801d9
KhunakonN <std.67122420121@ubru.ac.th>
2026-01-16 10:06:14 +0700

    add  edit_car and search_car template

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask ,render_template, request, redirect, url_for
+from flask import Flask ,render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = b'!@#$%$#@#$%^&^%$#$%^'
 
 cars = [
     {'id' : 1 , 'brand' : 'Toyota' , 'model' : 'Yaris Atuve' , 'year': 2024, 'price' : 620000},
@@ -28,13 +29,69 @@ def new_cars():
         price = int(request.form['price'])
         
         length = len(cars)
-        id = cars[length-1]['id'] + 1
+        if length>0:
+            id = cars[length-1]['id'] + 1
+        else:
+            id = 1
 
         car = {'id' : id , 'brand' : brand , 'model' : model , 'year': year , 'price' : price}
 
         cars.append(car)
+        flash('Add new car successfully','success')
 
         return redirect(url_for('show_cars'))
 
     return render_template('car/new_car.html',
                            title='New Car Page',)
+
+@app.route('/cars/<int:id>/edit', methods=['GET','POST'])
+def edit_car(id):
+    for c in cars:
+        if id == c['id']:
+            car = c
+            break
+
+    if request.method == 'POST':    # กด submit
+        brand = request.form['brand']
+        model = request.form['model']
+        year = int(request.form['year'])
+        price = int(request.form['price'])
+        # id = int(request.form['id'])
+
+        for car in cars:
+            if id == car['id']:
+                car['brand'] = brand
+                car['model'] = model
+                car['year'] = year
+                car['price'] = price
+                car['id'] = id
+                flash('Update car successfully','success')
+                break
+
+        return redirect(url_for('show_cars'))
+    
+    return render_template('car/edit_car.html',
+                           title='Edit Car Page',
+                           car=car)
+        
+@app.route('/cars/<int:id>/delete')
+def delete_car(id):
+    for car in cars:
+        if id == car['id']:
+            cars.remove(car)
+            flash('Delete car successfully','success')
+            break
+        
+    return redirect(url_for('show_cars'))
+
+@app.route('/cars/search')
+def search_car():
+  brand = request.args.get('brand')
+  tmp_cars = []
+  for car in cars:
+    if brand in car['brand']:
+      tmp_cars.append(car)
+  
+  return render_template('car/search_cars.html',
+                         title='Search Car Page',
+                         cars=tmp_cars)
